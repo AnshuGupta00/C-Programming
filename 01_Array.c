d159145ec5307c5c4ca513458518ad5b0bab76af
Anshu Gupta <anshu.gupta351613@gmail.com>
2026-04-15 22:33:21 +0530

    Initial commit

---
+++ 01_Array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+    int n;
+    
+    printf("Enter the size:");
+    scanf("%d",&n);
+    
+    int arr[n];
+    
+    printf("Enter the elements:");
+    for(int i =0; i< n; i++){
+        scanf("%d",&arr[i]);
+    }
+    
+    printf("Printing all the Array Element. \n ");
+    for(int i =0; i<n; i++){
+        printf("%d \t",arr[i]);
+    }
+    
+    return 0;
+}
