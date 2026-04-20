6c5e83fd0cb7c8d90574dbfdc074c964e5dce20e
Anshu Gupta <anshu.gupta351613@gmail.com>
2026-04-20 16:19:38 +0530

    InsertAtBegin

---
+++ InsertAtBegin.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct node{
+    int data;
+    struct node *next;
+};
+
+
+void main(){
+struct node *head;
+};
+struct node *head,*newnode;
+
+newnode =(struct node*) malloc(sizeof(structnode)); 
+
+printf("Enter the data waht you want to insert at the begining: ");
+scanf("%d",&newnode->node);
+
+newnode->next=head;
+head=newnode;
+}
